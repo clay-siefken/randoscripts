36ad3d0bf9707b0428da55248c20e86f2894a7fe
Clay Siefken <github@siefken.us>
2020-05-25 11:57:54 -0500

    bug in folders.sh

--- src/folders.sh
+++ src/folders.sh
@@ -1,5 +1,5 @@
 curl -X POST https://api.dropboxapi.com/2/files/list_folder \
-  --silent
+  --silent \
   --header "Authorization: Bearer $DROPBOX_TOKEN" \
   --header 'Content-Type: application/json' \
   --data '{"path":""}'
