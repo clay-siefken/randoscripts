06740c3f4752034e0bb1ea506712b32d6877e73e
Clay Siefken <github@siefken.us>
2020-05-28 22:44:24 -0500

    change order of args, support overwrite

--- src/upload.sh
+++ src/upload.sh
@@ -2,6 +2,7 @@
 curl -X POST https://content.dropboxapi.com/2/files/upload\
   --header "Authorization: Bearer $DROPBOX_TOKEN" \
   --header "Content-Type: application/octet-stream" \
-  --header "Dropbox-API-Arg: {\"path\":\"$1\"}" \
+  --header "Dropbox-API-Arg: {\"path\":\"$1\", \"mode\":\"overwrite\"}" \
+  --silent \
   --upload-file $2 \
-  --silent
+> /dev/null
